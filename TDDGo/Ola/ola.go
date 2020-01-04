c9821db580ef48432e6688aec464e6f889113906
Theophratus <fabiossilva00@gmail.com>
2020-01-04 12:15:15 -0300

    Inclusão de verificação de idioma com testes

--- TDDGo/Ola/ola.go
+++ TDDGo/Ola/ola.go
@@ -3,10 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Ola("Eu"))
+	fmt.Println(Ola("Eu", "pt"))
 }
 
+const prefixoOlaPortugues = "Olá, "
+const prefixoOlaEspanhol = "Holá, "
+const prefixoOlaFrances = "Bonjour, "
+const prefixoOlaIngles = "Hello"
+
 // Ola - Comentario
-func Ola(nome string) string {
-	return "Olá, " + nome
+func Ola(nome, idioma string) string {
+	if nome == "" {
+		nome = "mundo"
+	}
+	return verificaIdioma(idioma) + nome
+}
+
+func verificaIdioma(idioma string) (saudacao string) {
+	switch idioma {
+	case "es":
+		saudacao = prefixoOlaEspanhol
+	case "fr":
+		saudacao = prefixoOlaFrances
+	case "en":
+		saudacao = prefixoOlaIngles
+	default:
+		saudacao = prefixoOlaPortugues
+	}
+	return
 }
--- TDDGo/Ola/ola_test.go
+++ TDDGo/Ola/ola_test.go
@@ -3,10 +3,45 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	resultado := Ola("Eu")
-	esperado := "Olá, Eu"
 
-	if resultado != esperado {
-		t.Errorf("Resultado '%s', Esperado '%s'", resultado, esperado)
+	verificaMensagem := func(t *testing.T, resultado, esperado string) {
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("Resultado '%s', Esperado '%s'", resultado, esperado)
+		}
 	}
+
+	t.Run("Diz Olá para as pessoas em portugues", func(t *testing.T) {
+		resultado := Ola("Eu", "pt")
+		esperado := "Olá, Eu"
+		verificaMensagem(t, resultado, esperado)
+	})
+
+	t.Run("Diz olá para as pessoas em espanhol", func(t *testing.T) {
+		resultado := Ola("Eu", "es")
+		esperado := "Holá, Eu"
+
+		verificaMensagem(t, resultado, esperado)
+	})
+
+	t.Run("Diz olá para as pessoas em francês", func(t *testing.T) {
+		resultado := Ola("Eu", "fr")
+		esperado := "Bonjour, Eu"
+
+		verificaMensagem(t, resultado, esperado)
+	})
+
+	t.Run("Diz olá para as pessoas em inglês", func(t *testing.T) {
+		resultado := Ola("Eu", "en")
+		esperado := "Hello, Eu"
+
+		verificaMensagem(t, resultado, esperado)
+	})
+
+	t.Run("Diz 'Olá, mundo' quando uma string vazia for passada", func(t *testing.T) {
+		resultado := Ola("", "")
+		esperado := "Olá, mundo"
+
+		verificaMensagem(t, resultado, esperado)
+	})
 }
